220b52755c5a90cfca09bdb5e5d93c7dcbe6d32c
young-zy <youngzhaozhou@gmail.com>
2018-03-19 21:28:11 +0800

    补充2-34遇到的问题

--- 3-19_practice/2-34.cpp
+++ 3-19_practice/2-34.cpp
@@ -3,6 +3,11 @@
 
 using namespace std;
 
+/*
+    在使用string的过程中发现会出现程序无法定位入口 于动态链接库的情况
+    原因：电脑安装多个编译器，在运行过程中调用了错误的libc++.dll
+    解决方法：将编译器的libc++.dll（位于安装目录bin下）复制至exe文件目录下
+*/
 int main(){
     string str[5]={"red","yellow","blue","white","balck"};
     int i,j,k,l;
